3a39c23821c172c56a1c786d09c8bf5368228975
HiROJP <zeal1167@gmail.com>
2019-09-05 15:23:05 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: 'posts#posts'
+  get 'posts', to: 'posts#posts'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   get 'signup', to: 'users#new'
-  get 'posts', to: 'posts#posts'
   resources :users, only: [:index, :show, :new, :create]
 end
